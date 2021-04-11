721d292ecb4a583790ff38a0bff773d34067e125
Tudor Craciun <tudor.craciun1@gmail.com>
2021-04-11 11:18:49 +0300

    Implemented second chart inside COMPARE, modularized the javascript function, implemented image export (only PNG for now).

--- homeChart.js
+++ homeChart.js
@@ -1,6 +1,29 @@
-    let mainChart = document.getElementById("mainChart").getContext('2d');
+var MIME_TYPE = "image/png";
 
-    new Chart(mainChart, {
+function updateType(imageFormatBox) {
+    alert(imageFormatBox.value)
+    switch(imageFormatBox.value) {
+        case "CSV":
+            MIME_TYPE = "text/csv"
+            break;
+        case "PNG":
+            MIME_TYPE = "image/png"
+            break;
+        case "SCV":
+            MIME_TYPE = "image/scv"
+            break;
+        default:
+        // code block
+    }
+    alert(MIME_TYPE)
+}
+
+function generateChart(chartID) { //This will also take data as arguments
+    let mainChart = document.getElementById(chartID).getContext('2d');
+    let myChart = new Chart(mainChart, {
+        exportFileName: "obesityChart",
+        exportEnabled: true,
+        animationEnabled: true,
         type: 'bar',
         data: {
             labels: ['Romania', 'USA', 'Germany'],
@@ -20,3 +43,19 @@
         },
         options: {},
     })
+}
+
+function saveChart() {
+    var canvas = document.getElementById("mainChart");
+    var MIME_TYPE = "image/png";
+    var imageURL = canvas.toDataURL(MIME_TYPE);
+
+    var dlLink = document.createElement('a');
+    dlLink.download = "fileName";
+    dlLink.href = imageURL;
+    dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(':');
+
+    document.body.appendChild(dlLink);
+    dlLink.click();
+    document.body.removeChild(dlLink);
+}
