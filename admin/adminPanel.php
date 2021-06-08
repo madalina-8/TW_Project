a1ce30bdae169579f04190f8fb45fe3e05307182
Dulhac Mădălina <62876308+madalina-8@users.noreply.github.com>
2021-06-09 01:37:13 +0300

    Update adminPanel.php

--- admin/adminPanel.php
+++ admin/adminPanel.php
@@ -6,6 +6,8 @@
 
 ?>
 
+<?php require_once 'process.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,6 +35,51 @@
 
 ?>     
 
+
+    <?php
+
+    $mysqli = new mysqli("localhost","root","","project") or die(mysqli_error($mysqli));
+    $result = $mysqli->query("SELECT * FROM modify_status1") or die($mysqli->error);
+    //create a while so I can iterate through database and display every record
+    //pre_r($result->fetch_assoc()); //this function shows only the first record
+    
+    function pre_r( $array ) {
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
+    
+    ?>
+
+
+    <div class="row">
+         <table class="fields">
+             <thead>
+                 <tr> 
+                     <th> Field </th>
+                     <th> Status </th>
+                     <th> Action </th>
+                 </tr>
+            </thead>
+
+            <?php
+
+             while($row = $result->fetch_assoc()): ?>
+             <tr> 
+                 <td> <?php echo $row['Field']; ?> </td>
+                 <td> <?php echo $row['Status']; ?> </td>
+                 <td> 
+                     <div class = "btn-group">
+                         <a href="adminPanel.php?enable=<?php echo $row['id'];?>">Enable</a>  
+                         <a href="adminPanel.php?disable=<?php echo $row['id'];?>">Disable</a>  
+                    </div>
+                 </td>
+             </tr>
+
+            <?php endwhile; ?>
+
+         </table>
+    </div>
 
 </body>
 </html>
