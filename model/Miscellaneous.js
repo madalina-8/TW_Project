8d3b3c012b6078916fcb1e6f55be76333d508e97
Tudor Craciun <tudor.craciun1@gmail.com>
2021-06-19 20:00:51 +0300

    MEsaj generic

--- model/Miscellaneous.js
+++ model/Miscellaneous.js
@@ -1,7 +1,6 @@
 export default class Miscellaneous {
     constructor() {
         this.MIME_TYPE = "image/png"
-        this.testFileName = "text.csv"
         this.dataFileName = "data.csv"
         this.mainChartNameId = "mainChart"
     }
